593b45cac1abd8a4bb5e3551323db3dfe8496715
imran khan <imrankhan.ca2020@gmail.com>
2025-10-04 23:48:24 +0530

    Render base path fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: "./", // 👈 this line is important for Render/Netlify etc.
 })
